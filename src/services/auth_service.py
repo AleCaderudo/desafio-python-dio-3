7ef88e64dbf9a5f8d8ca4d97b592d5b81501b193
Carlos Alexandre da Motta <139296557+AleCaderudo@users.noreply.github.com>
2025-12-05 16:47:03 -0300

    Add files via upload

--- src/services/auth_service.py
+++ src/services/auth_service.py
@@ -1,8 +1,9 @@
-# src/services/auth_service.py
 from src.models import tables
 from src.database import database
-from src.utils.security import hash_password, verify_password, create_access_token
+from src.security import hash_password, verify_password, create_access_token
 from src.schemas.auth import UserCreate
+import jwt
+import uuid
 
 async def create_user(payload: UserCreate):
     query = tables.users.select().where(tables.users.c.username == payload.username)
@@ -29,5 +30,13 @@ async def authenticate_user(username: str, password: str):
 
 async def create_token_for_user(username: str):
     # subject can be username
-    token = create_access_token(subject=username)
+    token = create_access_token(user_id=username)
+    return token
+
+def create_access_token(subject: str) -> str:
+    payload = {
+        "sub": subject,
+        "jti": uuid4().hex,
+    }
+    token = jwt.encode(payload, SECRET, algorithm=ALGORITHM)
     return token
